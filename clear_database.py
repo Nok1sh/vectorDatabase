21ecee2da8531d379026527422b826cadd6ef5bb
Nokish <semenovykh.2006@mail.ru>
2025-10-24 16:00:00 +0500

    fix: add function/method for python files

--- clear_database.py
+++ clear_database.py
@@ -1,10 +1,11 @@
 import shutil
 
-path = "/Users/a1111/Desktop/Projects/vector_database/indicators_vector_database"
-try:
-    shutil.rmtree(path)
-    print(f"Папка {path} удалена.")
-except FileNotFoundError:
-    print(f"Папка {path} не существует.")
-except PermissionError:
-    print(f"Ошибка: Нет прав для удаления {path}.")
+def clear_database():
+    path = "indicators_vector_database"
+    try:
+        shutil.rmtree(path)
+        print(f"Папка {path} удалена.")
+    except FileNotFoundError:
+        print(f"Папка {path} не существует.")
+    except PermissionError:
+        print(f"Ошибка: Нет прав для удаления {path}.")
